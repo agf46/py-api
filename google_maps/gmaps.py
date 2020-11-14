a25e5bf34be725449f0bd4369f2b01ca700ca6b0
agf46 <akshaygf@gmail.com>
2020-11-14 12:15:37 -0800

    Fix flake8 and black tests

--- google_maps/gmaps.py
+++ google_maps/gmaps.py
@@ -1,20 +1,21 @@
-import requests 
-import pprint 
-import sys 
-import pandas as pd 
-# Script to find co-ordinates of the coloseum in Rome 
+import requests
+import pprint
+import sys
+import pandas as pd
+
+# Script to find co-ordinates of the coloseum in Rome
 API_KEY = sys.argv[0]
 
 API_ENDPOINT = "http://maps.googleapis.com/maps/api/geocode"
-KEY = API_KEY #Enter API KEY here 
-ADDR_LIST = pd.read_csv('rome_landmarks.csv', sep='|')
-coordinates = [] 
+KEY = API_KEY # Enter API KEY here
+ADDR_LIST = pd.read_csv("rome_landmarks.csv", sep="|")
+coordinates = []
 
-for ADDR in ADDR_LIST: 
-    ADDR = ADDR.replace(' ', '+')
+for ADDR in ADDR_LIST:
+    ADDR = ADDR.replace(" ", "+")
     RES = requests.get(f"{API_ENDPOINT}/json?address={ADDR}&key={KEY}").json()
-    LAT = RES['results'][0]['geometry']['location']['lat']
-    LONG = RES['results'][0]['geometry']['location']['lng']
-    coordinates.append((LAT,LONG))
-# Done 
+    LAT = RES["results"][0]["geometry"]["location"]["lat"]
+    LONG = RES["results"][0]["geometry"]["location"]["lng"]
+    coordinates.append((LAT, LONG))
+# Done
 
