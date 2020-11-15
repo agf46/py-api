f98e381d48ee73f164f191c9fcef081f6021b1e2
agf46 <akshaygf@gmail.com>
2020-11-14 17:27:38 -0800

    Trimming trailing whitespace

--- slack/slack_message.py
+++ slack/slack_message.py
@@ -1,13 +1,13 @@
-import os 
+import os
 from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError 
+from slack_sdk.errors import SlackApiError
 
 client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
 
-try: 
+try:
     response = client.chat_postMessage(channel="#test", text="SUP!")
     #assert response["message"]["text"] == "Hello World!"
-except SlackApiError as e: 
+except SlackApiError as e:
     assert e.response["ok"] is False
-    assert e.response["error"] 
+    assert e.response["error"]
     print(f"got an error: {e.response['error']}")
--- slack/slack_sysinfo.py
+++ slack/slack_sysinfo.py
@@ -1,9 +1,9 @@
 import platform
-import time 
-from tqdm import tqdm 
-import os 
+import time
+from tqdm import tqdm
+import os
 from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError 
+from slack_sdk.errors import SlackApiError
 from slack_print import SlackPrint
 
 host_system = platform.uname()
@@ -13,9 +13,9 @@ print(f"Analyzing system internals")
 for i in tqdm(host_system):
     time.sleep(1)
 
-with open("sysinfo.txt", "w") as text_file: 
+with open("sysinfo.txt", "w") as text_file:
     text_file.write(f"System: {host_system.system}\n")
-    text_file.write(f"Node Name: {host_system.node}\n") 
+    text_file.write(f"Node Name: {host_system.node}\n")
     text_file.write(f"Release: {host_system.release}\n")
     text_file.write(f"Version: {host_system.version}\n")
     text_file.write(f"Machine: {host_system.machine}\n")
@@ -25,12 +25,12 @@ with open("sysinfo.txt", "w") as text_file:
 client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
 
 
-try: 
+try:
     filepath="./sysinfo.txt"
     #response = client.files_upload(channel="#test", file=filepath)
-    sp=SlackPrint('SLACK_TOKEN', '#test') # Enter the slack token here 
+    sp=SlackPrint('SLACK_TOKEN', '#test') # Enter the slack token here
     sp.upload('sysinfo.txt')
-except SlackApiError as e: 
+except SlackApiError as e:
     assert e.response["ok"] is False
-    assert e.response["error"] 
+    assert e.response["error"]
     print(f"got an error: {e.response['error']}")
